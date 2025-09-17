7716315746e4b186d37a3594e4a47ae50c9dd3bd
Fyra Nika Dudas <fyrafillerdudas@gmail.com>
2025-09-17 21:54:54 +0800

    updated

--- app/config/database.php
+++ app/config/database.php
@@ -61,9 +61,9 @@ $database['main'] = array(
     'driver'	=> 'mysql',
     'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'sql12798085',
-    'password'	=> 'VGDmHmCZfA',
-    'database'	=> 'sql12798085',
+    'username'	=> 'sql12798993',
+    'password'	=> 'ndQPyFmBAy',
+    'database'	=> 'sql12798993',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
