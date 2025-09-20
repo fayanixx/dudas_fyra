f3d9d1f37c548986b3a11a57a8e70899603e58b1
Fyra Nika Dudas <fyrafillerdudas@gmail.com>
2025-09-20 20:00:01 +0800

    test again

--- app/views/users/view.php
+++ app/views/users/view.php
@@ -211,12 +211,11 @@ img.profile-img { border-radius: 50%; object-fit: cover; width:50px; height:50px
                         <td><?= $user['id'] ?></td>
                         <td>
                             <?php if(!empty($user['image_path'])): ?>
-                                <img src="<?= base_url('public/uploads/'.$user['image_path']) ?>">
-                                     alt="<?= htmlspecialchars($user['username']) ?>'s profile" 
-                                     class="profile-img">
+                                <img src="<?= base_url('public/uploads/'.$user['image_path']) ?>" 
+                                alt="<?= htmlspecialchars($user['username']) ?>'s profile" class="profile-img" />
                             <?php else: ?>
                                 <img src="<?= base_url('public/default-avatar.png') ?>" 
-                                     alt="Default profile" class="profile-img">
+                                alt="Default profile" class="profile-img" />
                             <?php endif; ?>
                         </td>
                         <td><?= $user['username'] ?></td>
